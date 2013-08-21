caaf7a139d60c0b7f7ead8a3099a9e0d61679dfa
PaulaEstrada <pauestra23@hotmail.com>
2013-08-21 17:13:48 +0200

    pagina final
    pagina final

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,3 +1,9 @@
-function pulsado(elemento) {
- alert("Has pulsado!!!!");
-}
+<script>
+var f=new Date();
+var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+document.write('<div class="mifecha">');
+document.write('<div class="ano">' + f.getFullYear() + '</div>');
+document.write('<div class="dia">' + f.getDate() + '</div>');
+document.write('<div class="mes">' + meses[f.getMonth()] + '</div>');
+document.write('</div>');
+</script>
